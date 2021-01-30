c6f37f7093806422b0087ec5342773f377e0f440
elffaW <elffaW@users.noreply.github.com>
2021-01-30 12:15:21 -0500

    add helper text to replay parser

--- parseNewToJson.sh
+++ parseNewToJson.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 source .env
 
+NOCOLOR='\033[0m'
+PURPLE='\033[0;35m'
+GREEN='\033[1;32m'
+RED='\033[1;31m'
+
 echo "Looking for new replays in [$REPLAY_LOC]"
 # echo $JSON_LOC
 
+numerror=0
+numsuccess=0
 # iterate over files in REPLAY_LOC
 #   parse REPLAY with rrrocket (changed to carball)
 #   move REPLAY somewhere else
@@ -12,13 +19,32 @@ if [[ -d "$REPLAY_LOC" && $(ls -A "$REPLAY_LOC") ]]; then
     for fullfile in "$REPLAY_LOC/"*.replay; do
         nopath=${fullfile##*/}
         file=${nopath%.*}
-        # echo "$file"
-        # ./rrrocket "$fullfile" > "$JSON_LOC/$file.json"
+        # echo "Parsing $fullfile"
+        #./rrrocket "$fullfile" > "$JSON_LOC/$file.json"
+
         carball -i "$fullfile" --json "$JSON_LOC/$file.json"
-        mv "$fullfile" "$JSON_LOC/parsed"
+        success=$?
+		if [ "$success" -eq "0" ]; then
+            echo
+            echo -e "${GREEN}[SUCCESS] ${NOCOLOR}PARSED $fullfile"
+            echo "... moving to $JSON_LOC/parsed"
+            echo
+			mv "$fullfile" "$JSON_LOC/parsed"
+            numsuccess=$((numsuccess+1))
+		else
+            echo
+			echo -e "${RED}[ERROR] ${NOCOLOR}FAILED PARSING $fullfile"
+			echo
+            numerror=$((numerror+1))
+		fi
         i=$((i+1))
     done
-    echo "Parsed $i replays with output in [$JSON_LOC]"
+    echo -e "Attempted to parse ${PURPLE}$i${NOCOLOR} replays"
+    echo -e "    ${GREEN}$numsuccess succeeded${NOCOLOR} with JSON output in [$JSON_LOC]"
+    echo -e "    ${RED}$numerror failed${NOCOLOR} and .replay files remain in [$REPLAY_LOC]"
 else
     echo "No new replays"
 fi
+
+echo
+
